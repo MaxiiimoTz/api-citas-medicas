0f87a101361ed5d585babf6098c97da4c289af06
Maximo <maxiiimo.tz@gmail.com>
2026-04-05 20:18:06 -0500

    feat: fix entity scanning and force table creation (MySQL)

--- src/main/java/com/idat/CitaApiApplication.java
+++ src/main/java/com/idat/CitaApiApplication.java
@@ -2,8 +2,10 @@ package com.idat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("com.idat.model")
 public class CitaApiApplication {
 
 	public static void main(String[] args) {
